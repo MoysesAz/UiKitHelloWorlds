328bafd1dab614ce6f489645afbf60a5d14a8bdb
Moyses Azevedo <m.mirazev@gmail.com>
2022-06-16 11:36:25 -0300

    :tada: init: Hello World

---
+++ HelloWord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWord
+//
+//  Created by Moyses Miranda do Vale Azevedo on 16/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
