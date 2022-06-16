393251a3a9c83d84d888624b83d3df313afd5b8d
Moyses Azevedo <m.mirazev@gmail.com>
2022-06-16 12:17:45 -0300

    :sparkles: feat: Adicionando CadastreController

---
+++ HelloWord/CadastreController.swift
@@ -0,0 +1,29 @@
+//
+//  CadastreController.swift
+//  HelloWord
+//
+//  Created by Moyses Miranda do Vale Azevedo on 16/06/22.
+//
+
+import UIKit
+
+class CadastreController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
