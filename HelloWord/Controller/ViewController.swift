515a8753560018f00aea1edcc6045b9f5e65de5f
Moyses Azevedo <m.mirazev@gmail.com>
2022-06-16 15:26:19 -0300

    :lipstick: feat: Adicionando funcionalidade ao buttonAction

--- HelloWord/Controller/ViewController.swift
+++ HelloWord/Controller/ViewController.swift
@@ -9,13 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var nameUser: UILabel!
-
     @IBOutlet weak var ageUser: UILabel!
     @IBOutlet weak var button: UIButton!
 
     @IBAction func buttonAction(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Cadastre", bundle: nil)
+        let secondVC = storyboard.instantiateViewController(identifier:"Cadastre") as! CadastreController
+        self.navigationController?.pushViewController(secondVC, animated: true)
+
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
