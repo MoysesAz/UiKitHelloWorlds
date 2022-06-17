ca0acaa09e27e1f27fa30a0e20766f637860991a
Moyses Azevedo <m.mirazev@gmail.com>
2022-06-17 16:34:27 -0300

    :lipstick: feat: Adicionando referências para a controller

---
+++ HelloWord/Scene1Controller.swift
@@ -0,0 +1,29 @@
+//
+//  Scene1Controller.swift
+//  HelloWord
+//
+//  Created by Moyses Miranda do Vale Azevedo on 17/06/22.
+//
+
+import UIKit
+
+class Scene1Controller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
