c30f5eb11bf41785d1efa6b15f19a01ae59038f8
Moyses Azevedo <m.mirazev@gmail.com>
2022-06-17 17:36:34 -0300

    :lipstick: feat: Adicionando funcionalidades de navegação

--- HelloWord/Control/ViewController.swift
+++ HelloWord/Control/ViewController.swift
@@ -15,10 +15,15 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonScene2(_ sender: Any) {
-
+        let storyboard = UIStoryboard(name: "Scene2", bundle: nil)
+        let secondVC = storyboard.instantiateViewController(identifier:"Scene2") as! Scene2Controller
+        self.navigationController?.pushViewController(secondVC, animated: true)
     }
 
-    @IBAction func buttonSecen2(_ sender: Any) {
+    @IBAction func buttonScene3(_ sender: Any) {
+        let storyboard = UIStoryboard(name: "Scene3", bundle: nil)
+        let secondVC = storyboard.instantiateViewController(identifier:"Scene3") as! Scene3Controller
+        self.navigationController?.pushViewController(secondVC, animated: true)
         
     }
 
