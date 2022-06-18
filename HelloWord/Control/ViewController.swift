af8e29b72abe1aeedc2283254134e9e062dd4137
Moyses Azevedo <m.mirazev@gmail.com>
2022-06-17 22:25:58 -0300

    :recycle: refactor: Modificando a forma de navegação dos buttonsScenes

--- HelloWord/Control/ViewController.swift
+++ HelloWord/Control/ViewController.swift
@@ -11,20 +11,27 @@ class ViewController: UIViewController {
     @IBAction func buttonScene1(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Scene1", bundle: nil)
         let secondVC = storyboard.instantiateViewController(identifier:"Scene1") as! Scene1Controller
-        self.navigationController?.pushViewController(secondVC, animated: true)
+        self.present(secondVC, animated: true)
+        self.dismiss(animated: true)
     }
 
     @IBAction func buttonScene2(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Scene2", bundle: nil)
-        let secondVC = storyboard.instantiateViewController(identifier:"Scene2") as! Scene2Controller
-        self.navigationController?.pushViewController(secondVC, animated: true)
-    }
-
-    @IBAction func buttonScene3(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Scene3", bundle: nil)
         let secondVC = storyboard.instantiateViewController(identifier:"Scene3") as! Scene3Controller
         self.navigationController?.pushViewController(secondVC, animated: true)
-        
+        self.navigationController?.popViewController(animated: true)
+    }
+
+    @IBAction func buttonScene3(_ sender: Any) {
+
+        let storyboard = UIStoryboard(name: "Scene2", bundle: nil)
+        let secondVC = storyboard.instantiateViewController(identifier:"Scene2") as! Scene2Controller
+        self.show(secondVC, sender: true)
+        if self.navigationController != nil {
+            self.navigationController?.popViewController(animated: true)
+        } else {
+            self.dismiss(animated: true)
+        }
     }
 
     override func viewDidLoad() {
